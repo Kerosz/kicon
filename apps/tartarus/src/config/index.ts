5bd22ea4cdc2400e8bfd6a62f532cec937294f5b
Kerosz <andrei_chirila@outlook.com>
2021-05-30 22:57:23 +0300

    chore(tartarus): changed dependencies of commitlint/husky/changelog from apps to global workspace && connected db
    Fixed a mistake where dependencies for commitlint, jusky and lerna changelog were added to the apps not the main workspace.
    Also added new env variables and updated config in order to be able to connect to postgres db

--- apps/tartarus/src/config/index.ts
+++ apps/tartarus/src/config/index.ts
@@ -12,12 +12,23 @@ if (envFound.error) {
 export default {
   /** Application host */
   host: process.env.HOSTNAME,
+
   /** Application running port */
   port: parseInt(process.env.PORT as string, 10),
+
   /** Current application environment */
   env: process.env.NODE_ENV,
+
   /** API configs */
   api: {
     prefix: "/api/v1",
   },
+
+  /** Postgres Configuration */
+  postgres: {
+    host: process.env.POSTGRES_HOST,
+    db: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  }
 } as const;
